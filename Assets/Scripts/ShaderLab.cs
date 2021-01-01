3aba7d94e99aa28679d855cdc741511b31f55e10
Aidan <AidanofVT@gmail.com>
2021-01-01 14:43:21 -0500

    Shader done???

--- Assets/Scripts/ShaderLab.cs
+++ Assets/Scripts/ShaderLab.cs
@@ -21,62 +21,36 @@ public class ShaderLab : MonoBehaviour {
     public Texture2D third;
     public Texture2D fourth;
     Texture2D myTexture;
-
     Texture2D [] tileLibrary;
     public int spriteSize = 128;
     public int[,] mapData;
-
     public ComputeShader myShader;
     int kernelNumber;
-    RenderTexture CPUside;
     float [] bugger = new float [128 * 128];
-    Color32[] pixelsOut = new Color32[128 * 128];
+    Color32[] pixelsOut;
+    RawImage rawImageComponent;
+    int pixelsWide;
+    int pixelsTall;
+    float aspectRatio;
+    ComputeBuffer libraryBuffer;
+    ComputeBuffer worldBuffer;
+    ComputeBuffer widthHeight;
+    ComputeBuffer cameraSpot;
+    ComputeBuffer scaleBuffer;
+    ComputeBuffer outputBuffer;
+    ComputeBuffer bugBuffer;
 
     void Start () {
-        makeMap(2, 2);
-        Color32 [] pixelsLibrary = libraryToArray();        
-        kernelNumber = myShader.FindKernel("action");
-        ComputeBuffer bugBuffer = new ComputeBuffer(spriteSize * spriteSize, 4);
-            myShader.SetBuffer(kernelNumber, "bugger", bugBuffer);
-        ComputeBuffer libraryBuffer = new ComputeBuffer(spriteSize * spriteSize * tileLibrary.Length, 4, ComputeBufferType.Default);
-            myShader.SetBuffer(kernelNumber, "imageLibrary", libraryBuffer);
-            libraryBuffer.SetData(pixelsLibrary);
-        ComputeBuffer worldBuffer = new ComputeBuffer(mapData.GetLength(0) * mapData.GetLength(1), sizeof(int));
-            myShader.SetBuffer(kernelNumber, "world", worldBuffer);
-            worldBuffer.SetData(mapData);
-        ComputeBuffer scaleBuffer = new ComputeBuffer(1, sizeof(float));
-            myShader.SetBuffer(kernelNumber, "scale", scaleBuffer);
-            float [] forGPU = new float[1] {Camera.main.orthographicSize};            
-            scaleBuffer.SetData(forGPU);
-        ComputeBuffer cameraSpot = new ComputeBuffer(1, sizeof(float) * 2);
-            myShader.SetBuffer(kernelNumber, "hereNow", cameraSpot);
-            cameraSpot.SetData(cameraPosAsArray());
-        ComputeBuffer outputBuffer = new ComputeBuffer(spriteSize * spriteSize, 4, ComputeBufferType.Default);
-            myShader.SetBuffer(kernelNumber, "output", outputBuffer);
-        myTexture = new Texture2D(spriteSize, spriteSize);
-        GetComponent<RawImage>().texture = myTexture;
-        // ComputeBuffer inputBuffer = new ComputeBuffer(128 * 128, sizeof(float) * 4);
-        // myShader.SetBuffer(kernelNumber, "input", inputBuffer);
-        // int x = (int) transform.parent.GetComponent<RectTransform>().sizeDelta.x;
-        // int y = (int) transform.parent.GetComponent<RectTransform>().sizeDelta.y;        
-        myShader.Dispatch(kernelNumber, 1, 1, 1);
-        outputBuffer.GetData(pixelsOut);
-        myTexture.SetPixels32(pixelsOut);
-        myTexture.Apply();
-        bugBuffer.GetData(bugger);
-        string debugOut = "Done: ";
-        for (int i = 0; i < 400; ++i) {
-            debugOut += bugger[i] + ", ";
-        }
-        Debug.Log(debugOut);
-    }
-
-    Color32 [] libraryToArray () {
-        Color32[] toReturn = new Color32 [tileLibrary.Length * 128 * 128];
-        for (int i = 0; i < tileLibrary.Length; ++i) {
-            tileLibrary[i].GetPixels32().CopyTo(toReturn, i * 128 * 128);
-        }
-        return toReturn;
+        makeMap(1000, 1000);
+        pixelsWide = (int) transform.parent.GetComponent<RectTransform>().sizeDelta.x;
+        pixelsTall = (int) transform.parent.GetComponent<RectTransform>().sizeDelta.y;
+        aspectRatio = (float) pixelsWide / (float) pixelsTall;
+        pixelsOut = new Color32[pixelsWide * pixelsTall]; 
+        myTexture = new Texture2D(pixelsWide, pixelsTall);
+        rawImageComponent = GetComponent<RawImage>();
+        rawImageComponent.texture = myTexture;
+        ShaderStart();
+        //UnleashShaderPower();
     }
 
     float[] cameraPosAsArray () {
@@ -85,6 +59,14 @@ public class ShaderLab : MonoBehaviour {
         toreturn[0] = where.x;
         toreturn[1] = where.y;
         return toreturn;
+    }
+
+    Color32 [] libraryToArray () {
+        Color32[] toReturn = new Color32 [tileLibrary.Length * 128 * 128];
+        for (int i = 0; i < tileLibrary.Length; ++i) {
+            tileLibrary[i].GetPixels32().CopyTo(toReturn, i * 128 * 128);
+        }
+        return toReturn;
     }
 
     void makeMap (int width, int height) {
@@ -110,6 +92,55 @@ public class ShaderLab : MonoBehaviour {
                 mapData[i,j] = 3;
             }
         }
+    }
+
+    void ShaderStart () {
+        kernelNumber = myShader.FindKernel("action");
+        libraryBuffer = new ComputeBuffer(spriteSize * spriteSize * tileLibrary.Length, 4, ComputeBufferType.Default);
+            myShader.SetBuffer(kernelNumber, "imageLibrary", libraryBuffer);
+            libraryBuffer.SetData(libraryToArray());
+        worldBuffer = new ComputeBuffer(mapData.GetLength(0) * mapData.GetLength(1), sizeof(int));
+            myShader.SetBuffer(kernelNumber, "world", worldBuffer);
+        widthHeight = new ComputeBuffer(1, sizeof(float) * 2);
+            myShader.SetBuffer(kernelNumber, "cameraDimensions", widthHeight);
+            widthHeight.SetData(new float[] {pixelsWide, pixelsTall});
+        cameraSpot = new ComputeBuffer(1, sizeof(float) * 2);
+            myShader.SetBuffer(kernelNumber, "hereNow", cameraSpot);            
+        scaleBuffer = new ComputeBuffer(1, sizeof(float) * 2);
+            myShader.SetBuffer(kernelNumber, "scale", scaleBuffer);            
+        outputBuffer = new ComputeBuffer(pixelsWide * pixelsTall, 4, ComputeBufferType.Default);
+            myShader.SetBuffer(kernelNumber, "output", outputBuffer);
+        bugBuffer = new ComputeBuffer(pixelsWide * pixelsTall, 4);
+            myShader.SetBuffer(kernelNumber, "bugger", bugBuffer);
+    }
+
+    void UnleashShaderPower () {
+        worldBuffer.SetData(mapData);        
+        cameraSpot.SetData(cameraPosAsArray());
+        float scale = Camera.main.orthographicSize;
+        scaleBuffer.SetData(new float[] {scale * aspectRatio, scale});
+        myShader.Dispatch(kernelNumber, 4, 4, 1);
+        outputBuffer.GetData(pixelsOut);
+        myTexture.SetPixels32(pixelsOut);
+        myTexture.Apply();
+        rawImageComponent.SetNativeSize();
+        // bugBuffer.GetData(bugger);
+        // string debugOut = "Done: ";
+        // for (int i = 0; i < 1000; ++i) {
+        //     debugOut += bugger[i] + ", ";
+        // }
+        // Debug.Log(debugOut);
+    }
+
+    void Update () {
+        Debug.Log("Frame time: " + Time.deltaTime);
+        if (Camera.main.orthographicSize == 150) {
+            Camera.main.orthographicSize = 300;
+        }
+        else {
+            Camera.main.orthographicSize = 150;
+        }
+        UnleashShaderPower();
     }
 
 }
